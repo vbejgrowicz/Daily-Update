28974078d860b129602592527dfa9b30953635ba
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-07-28 13:43:14 -0400

    Feature: Add default image for no API image

--- src/Article/ArticleImage.js
+++ src/Article/ArticleImage.js
@@ -4,10 +4,13 @@ import React from 'react';
 class ArticleImage extends React.Component {
 
   render() {
-    return (
+    return (this.props.images.length > 0) ? (
     <div className="image" style={{ width: 210, height: 140, backgroundImage: 'url(' + (this.props.images.length > 0 && this.props.images[3].url) +')'}}>
     </div>
-    );
+  ):(
+    <div className="no-image" style={{width: 210, height: 140}}>
+    </div>
+  );
   }
 }
 
